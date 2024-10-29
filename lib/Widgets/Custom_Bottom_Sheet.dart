719eea7802f7ebf917562ddfa9cf5458a7a1547b
AhmedHossamEissa1 <ahmedhossameissa65@gmail.com>
2024-10-29 19:08:14 +0300

    add bloc consumer to buttom sheet

--- lib/Widgets/Custom_Bottom_Sheet.dart
+++ lib/Widgets/Custom_Bottom_Sheet.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:notes_app/Cubets/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/Widgets/Custom_button.dart';
 import 'package:notes_app/Widgets/Custom_textField.dart';
 import 'package:notes_app/Widgets/add_form.dart';
@@ -31,8 +34,21 @@ class _CustomBottomSheetState extends State<CustomBottomSheet> {
         shrinkWrap: true, // Prevents the list from taking all available space
         children: [
           SizedBox(height: 32),
-          AddForm(),
-          
+          BlocConsumer<AddNoteCubit, AddNoteState>(
+            listener: (context, state) {
+              if (state is AddNoteSuccess) {
+                Navigator.pop(context);
+              }
+              if (state is AddNoteFail) {
+                print('failed');
+              }
+            },
+            builder: (context, state) {
+              return ModalProgressHUD(
+                  inAsyncCall: state is AddNoteLoading ? true : false,
+                  child: AddForm());
+            },
+          ),
         ],
       ),
     );
